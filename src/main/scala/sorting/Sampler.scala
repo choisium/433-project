95ae573fbf1a7d0841a84cd5f2c39964696ee098
parksbn812 <parksbn812@postech.ac.kr>
2021-11-29 19:55:18 +0900

    feat: refactor code with getLines.take(sampleSize:Int)

--- src/main/scala/sorting/Sampler.scala
+++ src/main/scala/sorting/Sampler.scala
@@ -1,10 +1,7 @@
 package sorting
 
-import org.json4s.JsonDSL.int2jvalue
-
 import java.io.{File, PrintWriter}
 import scala.io.Source
-import scala.util.control.Breaks.break
 
 object Sampler {
   def sample(workerPath: String, sampleSize: Int): Unit = {
@@ -12,16 +9,12 @@ object Sampler {
       val bufferedSource = Source.fromFile(workerPath + "/input/input-1")
       val sampleWriter = new PrintWriter(new File(workerPath + "/sample"))
 
-      var sampleCount: Int = 0
-      val lineOfInput = bufferedSource.getLines();
-
-      while (lineOfInput.hasNext && sampleCount < sampleSize) {
-        sampleWriter.write(lineOfInput.next() + "\n")
-        sampleCount += 1
+      for (line <- bufferedSource.getLines.take(sampleSize)) {
+        sampleWriter.write(line + "\n")
       }
 
       bufferedSource.close
-      sampleWriter.close()
+      sampleWriter.close
 
     } catch {
       case ex: Exception => println(ex)
