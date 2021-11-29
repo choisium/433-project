bbd3972e9e42db14c3d9d508827f1a653edf419d
parksbn812 <parksbn812@postech.ac.kr>
2021-11-29 13:34:18 +0900

    test: add test for sampling comparing contents of files

--- src/test/scala/sorting/SamplingTest.scala
+++ src/test/scala/sorting/SamplingTest.scala
@@ -3,12 +3,42 @@ package sorting
 import org.scalatest.funsuite.AnyFunSuite
 
 import java.io.File
+import scala.io.Source
 
 class SamplingTest extends AnyFunSuite {
-  Sampler.sample(30)
-  val projectPath: String = System.getProperty("user.dir")
-  val sampledFile = new File(projectPath + "/src/main/resources/1/sample")
+  val projectPath: String = System.getProperty("user.dir");
+  val workerPath: String = projectPath + "/src/main/resources/1"
+  val sampledFile = new File(workerPath + "/sample")
   val testSampledFile = new File(projectPath + "/src/test/scala/sorting/sample-test")
 
-  assert(sampledFile.length == testSampledFile.length)
+  Sampler.sample(workerPath, 30)
+  try {
+    val isContentsEqual: Boolean = isContentsOfSampleAndTestFileEqual(workerPath + "/sample", projectPath + "/src/test/scala/sorting/sample-test")
+    assert(isContentsEqual)
+    assert(sampledFile.length == testSampledFile.length)
+  } catch {
+    case ex: Exception => println(ex)
+  }
+
+  def isContentsOfSampleAndTestFileEqual(samplePath: String, testFilePath: String): Boolean = {
+    try {
+      val bufferedSample = Source.fromFile(samplePath)
+      val bufferedTestFile = Source.fromFile(testFilePath)
+
+      var sampleCount: Int = 0
+      val lineOfSample = bufferedSample.getLines();
+      val lineOfTestFile = bufferedTestFile.getLines();
+
+      while (lineOfSample.hasNext && lineOfTestFile.hasNext && sampleCount < 30) {
+        sampleCount += 1
+        if (!lineOfSample.next.equals(lineOfTestFile.next)) return false
+      }
+      bufferedSample.close
+      bufferedTestFile.close
+      sampleCount == 30
+    } catch {
+      case ex: Exception => println(ex)
+        false
+    }
+  }
 }
