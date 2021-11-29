05b4795eabb7f455cbc8b974ee74deb3d450daf4
parksbn812 <parksbn812@postech.ac.kr>
2021-11-29 20:01:49 +0900

    test: parametrize sampleSize and refactor comparing function

--- src/test/scala/sorting/SamplingTest.scala
+++ src/test/scala/sorting/SamplingTest.scala
@@ -6,36 +6,39 @@ import java.io.File
 import scala.io.Source
 
 class SamplingTest extends AnyFunSuite {
-  val projectPath: String = System.getProperty("user.dir");
-  val workerPath: String = projectPath + "/src/main/resources/1"
-  val sampledFile = new File(workerPath + "/sample")
-  val testSampledFile = new File(projectPath + "/src/test/scala/sorting/sample-test")
+  test("Sample file creation from worker's input") {
 
-  Sampler.sample(workerPath, 30)
-  try {
-    val isContentsEqual: Boolean = isContentsOfSampleAndTestFileEqual(workerPath + "/sample", projectPath + "/src/test/scala/sorting/sample-test")
-    assert(isContentsEqual)
-    assert(sampledFile.length == testSampledFile.length)
-  } catch {
-    case ex: Exception => assert(false)
+    val sampleSize = 30;
+    val projectPath: String = System.getProperty("user.dir");
+    val workerPath: String = projectPath + "/src/test/resources/worker"
+    val sampledFile = new File(workerPath + "/sample")
+    val testSampledFile = new File(projectPath + "/src/test/resources/sorting/sample-test")
+
+    Sampler.sample(workerPath, sampleSize)
+    try {
+      val isContentsEqual: Boolean = isContentsOfSampleAndTestFileEqual(workerPath + "/sample",
+        projectPath + "/src/test/resources/sorting/sample-test", sampleSize)
+      assert(sampledFile.length == testSampledFile.length)
+      assert(isContentsEqual)
+    } catch {
+      case ex: Exception => assert(false)
+    }
   }
 
-  def isContentsOfSampleAndTestFileEqual(samplePath: String, testFilePath: String): Boolean = {
+  def isContentsOfSampleAndTestFileEqual(samplePath: String, testFilePath: String, sampleSize: Int): Boolean = {
     try {
       val bufferedSample = Source.fromFile(samplePath)
       val bufferedTestFile = Source.fromFile(testFilePath)
 
-      var sampleCount: Int = 0
-      val lineOfSample = bufferedSample.getLines();
-      val lineOfTestFile = bufferedTestFile.getLines();
+      val linesOfSample = bufferedSample.getLines.take(sampleSize);
+      val linesOfTestFile = bufferedTestFile.getLines.take(sampleSize);
+      val isEqual = linesOfSample.sameElements(linesOfTestFile) && linesOfSample.size.equals(linesOfTestFile.size)
 
-      while (lineOfSample.hasNext && lineOfTestFile.hasNext && sampleCount < 30) {
-        sampleCount += 1
-        if (!lineOfSample.next.equals(lineOfTestFile.next)) return false
-      }
       bufferedSample.close
       bufferedTestFile.close
-      sampleCount == 30
+
+      isEqual
+
     } catch {
       case ex: Exception => println(ex)
         false
