fcc9d68b5fd1f0eb69f86c0af29ed63d55958622
parksbn812 <parksbn812@postech.ac.kr>
2021-12-09 14:29:36 +0900

    test: add merge test

--- src/test/scala/sorting/MergingTest.scala
+++ src/test/scala/sorting/MergingTest.scala
@@ -8,22 +8,36 @@ import java.io.File
 class MergingTest extends AnyFunSuite {
   val projectPath: String = System.getProperty("user.dir")
 
+  /**
+   * resources structure:
+   * requirements:
+   * ~/test/resources/simple/results/shuffle-result/output-#      : to compare with merged file
+   * ~/test/resources/simple/1/shuffle-#-#                        : input files, if workerId is equal to first #
+   *
+   * after test:
+   * new:
+   * ~/test/resources/simple/1/output/output-#                    : output files
+   */
   test("Merge files") {
+    val workerPath: String = projectPath + "/src/test/resources/simple/1"
+    val mergeResultPath: String = workerPath + "/../results/shuffle-result"
 
-  }
+    val mergePath0 = workerPath + "/output/output-0"
+    val mergePath1 = workerPath + "/output/output-1"
+    val mergePath2 = workerPath + "/output/output-2"
 
-  test("Sorts files") {
+    val mergeResultPath0 = workerPath + "/../results/shuffle-result/output-0"
+    val mergeResultPath1 = workerPath + "/../results/shuffle-result/output-1"
+    val mergeResultPath2 = workerPath + "/../results/shuffle-result/output-2"
 
-    val workerPath: String = projectPath + "/src/test/resources/simple/worker"
-    val partitionPath1: String = workerPath + "/partition-1-1-unsorted"
-    val partitionPath2: String = workerPath + "/partition-2-1-unsorted"
-    val partitionPath3: String = workerPath + "/partition-3-1-unsorted"
+    // keys start with 0 ~ 9
+    val subRanges = Seq(Tuple2(" ", "3"), Tuple2("4", "7"), Tuple2("8", "9"))
 
     try {
-      Sorter.sort(partitionPath1)
-      Sorter.sort(partitionPath2)
-      Sorter.sort(partitionPath3)
-
+      Merger.merge(workerPath, subRanges)
+      assert(isContentsOfSampleAndTestFileEqual(mergeResultPath0, mergePath0, new File(mergeResultPath0).length.toInt))
+      assert(isContentsOfSampleAndTestFileEqual(mergeResultPath1, mergePath1, new File(mergeResultPath1).length.toInt))
+      assert(isContentsOfSampleAndTestFileEqual(mergeResultPath2, mergePath2, new File(mergeResultPath2).length.toInt))
     } catch {
       case ex: Exception => assert(false)
     }
